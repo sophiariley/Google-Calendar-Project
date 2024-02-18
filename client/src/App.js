c1e2a4545be7a5dba6fa8cc52c5ed7419619f25e
Sophia Riley <riley.e.sophia@gmail.com>
2024-02-17 22:50:57 -0500

    Completed backend

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,9 @@ function App() {
   // Hooks
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [eventName, setEventName] = useState("");
+
+  const [eventDescription, setEventDescription] = useState("");
   const session = useSession(); // Current active token is stored inside of the session. When a session exits, we have a user
   const supabase = useSupabaseClient(); // Access client
   const { isLoading } = useSessionContext();
@@ -44,10 +47,49 @@ function App() {
     await supabase.auth.signOut();
   }
 
+  // Function to handle creating an event
+  // Documentation: https://developers.google.com/calendar/api/v3/reference/events/insert
+  async function createEvent() {
+    console.log("Event created"); // Debugging
+    const event = {
+      summary: eventName,
+      description: eventDescription,
+      start: {
+        dateTime: startDate.toISOString(),
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // Get time zone from user's machine
+      },
+      end: {
+        dateTime: endDate.toISOString(),
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // Get time zone from user's machine
+      },
+    };
+
+    // Fetch request
+    await fetch(
+      "https://www.googleapis.com/calendar/v3/calendars/primary/events",
+      {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + session.provider_token, // Google access token
+        },
+        body: JSON.stringify(event),
+      }
+    )
+      .then((data) => {
+        return data.json;
+      })
+      .then((data) => {
+        console.log(data); // Debugging
+        alert("Event created! Check Google Calendar.");
+      });
+  }
+
   // Debugging:
   console.log(session);
   console.log(startDate);
   console.log(endDate);
+  console.log(eventName);
+  console.log(eventDescription);
 
   return (
     <div className="App">
@@ -56,10 +98,21 @@ function App() {
         {session ? (
           <>
             <h2>Welcome back, {session.user.email}!</h2>
+            <p>Event Name</p>
+            <input type="text" onChange={(e) => setEventName(e.target.value)} />
+            <p>Event Description</p>
+            <input
+              type="text"
+              onChange={(e) => setEventDescription(e.target.value)}
+            />
             <p>Starts</p>
             <DateTimePicker value={startDate} onChange={setStartDate} />
             <p>Ends</p>
             <DateTimePicker value={endDate} onChange={setEndDate} />
+            <p />
+            <button onClick={() => createEvent()}>Create event</button>
+            <p />
+            <hr />
             <button onClick={() => signOut()}>Sign out</button>
           </>
         ) : (
