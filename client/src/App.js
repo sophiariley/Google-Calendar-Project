9a2456af36024b1e2a42a6ab7538535937cb8690
Sophia Riley <riley.e.sophia@gmail.com>
2024-02-17 21:47:28 -0500

    Handling sign in and out

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 function App() {
+  // Hooks
+  const session = useSession(); // Current active token is stored inside of the session. When a session exits, we have a user
+  const client = useSupabaseClient(); // Access client
+
+  // Function to handle signing into Google
+  async function googleSignIn() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        scopes: "https://www.googleapis.com/auth/calendar",
+      },
+    });
+
+    if (error) {
+      alert("There was an error logging in. Please try again.");
+      console.log(error);
+    }
+  }
+
+  // Function to handle signing out of Google
+  async function signOut() {
+    await supabase.auth.signOut();
+  }
+
+  console.log(session); // Debugging
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: "400px", margin: "30px auto" }}>
+        {/* If a session exists, show welcome text. Else, show login button*/}
+        {session ? (
+          <>
+            <h2>Welcome back, {session.user.email}!</h2>
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        ) : (
+          <>
+            <button onClick={() => googleSignIn()}>Sign in with Google</button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
